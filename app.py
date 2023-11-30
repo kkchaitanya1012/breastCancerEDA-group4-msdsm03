08ba615d32715f13be089208efd25d1da43fd74f
Chaitanya Kalluri <chaitanyakallurik@gmail.com>
2023-11-30 21:54:36 +0530

    added viz

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 import pygwalker as pyg
+import altair as alt
 from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
 
 
 st.set_page_config(
-    page_title="Breast Cancer Dashboard",
+    page_title="📖 Breast Cancer Dashboard",
     layout="wide"
 )
 
@@ -22,7 +23,7 @@ data = pd.read_csv(download_link)
 
 
 def about():
-    st.title("Breast Cancer Data EDA")
+    st.title("📖 Breast Cancer Data EDA")
 
     st.write("The Wisconsin Breast Cancer Dataset deals with the 32 features impact breast cancer and its diagnosis of 570 participants")
 
@@ -56,6 +57,7 @@ def tasks():
     st.dataframe(taskData, use_container_width = True)
 
 def dashboard():
+    
     init_streamlit_comm()
     @st.cache_resource
     def get_pyg_html(df: pd.DataFrame) -> str:
@@ -69,11 +71,59 @@ def dashboard():
 
     components.html(get_pyg_html(df), width=1300, height=1000, scrolling=True)
 
+def analyze():
+    st.title("Visualisations and Analysis")
+    st.divider()
+    st.header("Class Distrubution")
+    st.bar_chart(data['diagnosis'].value_counts())
+    imbalance = data['diagnosis'].value_counts()[0]/data['diagnosis'].value_counts()[1]
+    st.write("The dataset is imbalanced with a ratio of 1:", imbalance)
+    st.divider()
+    st.header("Outlier and Distribution Analysis with respect to Diagnosis")
+    parameters = st.selectbox("Select a parameter", data.columns)
+    boxplot = alt.Chart(data).mark_boxplot().encode(
+        x='diagnosis',
+        y=parameters,
+        color='diagnosis'
+    ).properties(
+        width=700,
+        height=500,
+        title="Outlier and Distribution Analysis with respect to Diagnosis",
+        
+    )
+    st.altair_chart(boxplot,theme="streamlit")
+    st.divider()
+    st.header("Pair Plot")
+    st.write("The pair plot shows the relationship between the different features")
+    cols = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean',	'concavity_mean', 'diagnosis']
+    pairplot = alt.Chart(data[cols]).mark_circle().encode(
+        alt.X(alt.repeat("column"), type='quantitative'),
+        alt.Y(alt.repeat("row"), type='quantitative'),
+        color='diagnosis:N'
+    ).properties(
+        width=150,
+        height=150
+    ).repeat(
+        row=['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean'],
+        column=['smoothness_mean', 'compactness_mean',	'concavity_mean']
+    )
+    st.altair_chart(pairplot)
+    st.divider()
+    st.header("Correlation Matrix")
+    st.write("The correlation matrix shows the correlation between the different features")
+    corr = data.corr()
+    st.dataframe(corr)
+    st.divider()
+    st.header("Correlation Bar Chart")
+    st.write("The correlation bar chart shows the correlation between the different features")
+    
+
 
 page_names_to_funcs = {
     "About": about,
     "Tasks": tasks,
-    "Dashboard" : dashboard
+    "📈 Analysis and Viz": analyze,
+    "Dashboard Playground ⏯️" : dashboard
 }
 
 demo_name = st.sidebar.selectbox("Choose a page", page_names_to_funcs.keys())
